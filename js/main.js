6debd8d52d38f07191ab4925741b3b366fc33c3b
Dawam Raja <akunnyadawam9@gmailcom>
2023-11-08 11:20:28 +0700

    myid

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@
 * Version: 1.2.0
 */
 
-(function($) {
-"use strict";
+(function ($) {
+    "use strict";
     // Portfolio subpage filters
     function portfolio_init() {
         var portfolio_grid = $('.portfolio-grid'),
             portfolio_filter = $('.portfolio-filters');
-            
+
         if (portfolio_grid) {
 
             portfolio_grid.shuffle({
@@ -24,7 +24,7 @@
                 e.preventDefault();
                 $('.portfolio-filters .filter').parent().removeClass('active');
                 $(this).parent().addClass('active');
-                portfolio_grid.shuffle('shuffle', $(this).attr('data-group') );
+                portfolio_grid.shuffle('shuffle', $(this).attr('data-group'));
             });
 
         }
@@ -40,7 +40,7 @@
         if (windowWidth < 1025) {
             siteHeader.addClass('mobile-menu-hide');
             $('.menu-toggle').removeClass('open');
-            setTimeout(function(){
+            setTimeout(function () {
                 siteHeader.addClass('animate');
             }, 500);
         } else {
@@ -53,11 +53,11 @@
     function customScroll() {
         var windowWidth = $(window).width();
         if (windowWidth > 1024) {
-            $('.animated-section, .single-page-content').each(function() {
+            $('.animated-section, .single-page-content').each(function () {
                 $(this).perfectScrollbar();
             });
         } else {
-            $('.animated-section, .single-page-content').each(function() {
+            $('.animated-section, .single-page-content').each(function () {
                 $(this).perfectScrollbar('destroy');
             });
         }
@@ -77,8 +77,7 @@
                     type: "POST",
                     url: url,
                     data: $(this).serialize(),
-                    success: function (data)
-                    {
+                    success: function (data) {
                         var messageAlert = 'alert-' + data.type;
                         var messageText = data.message;
 
@@ -97,9 +96,9 @@
 
     //On Window load & Resize
     $(window)
-        .on('load', function() { //Load
+        .on('load', function () { //Load
             // Animation on Page Loading
-            $(".preloader").fadeOut( 800, "linear" );
+            $(".preloader").fadeOut(800, "linear");
 
             // initializing page transition.
             var ptPage = $('.animated-sections');
@@ -109,9 +108,9 @@
                 });
             }
         })
-        .on('resize', function() { //Resize
-             mobileMenuHide();
-             $('.animated-section').each(function() {
+        .on('resize', function () { //Resize
+            mobileMenuHide();
+            $('.animated-section').each(function () {
                 $(this).perfectScrollbar('update');
             });
             customScroll();
@@ -119,11 +118,11 @@
 
 
     // On Document Load
-    $(document).on('ready', function() {
+    $(document).on('ready', function () {
         var movementStrength = 23;
         var height = movementStrength / $(document).height();
         var width = movementStrength / $(document).width();
-        $("body").on('mousemove', function(e){
+        $("body").on('mousemove', function (e) {
             var pageX = e.pageX - ($(document).width() / 2),
                 pageY = e.pageY - ($(document).height() / 2),
                 newvalueX = width * pageX * -1,
@@ -135,7 +134,7 @@
                 "background-position": "calc( 50% + " + newvalueX + "px ) calc( 50% + " + newvalueY + "px )",
             });
 
-            setTimeout(function() {
+            setTimeout(function () {
                 elements.removeClass('transition');
             }, 300);
         })
@@ -165,7 +164,7 @@
 
         // Blog grid init
         var $container = $(".blog-masonry");
-        $container.imagesLoaded(function(){
+        $container.imagesLoaded(function () {
             $container.masonry();
         });
 
@@ -186,7 +185,7 @@
         });
 
         // Testimonials Slider
-        $(".testimonials.owl-carousel").imagesLoaded(function(){
+        $(".testimonials.owl-carousel").imagesLoaded(function () {
             $(".testimonials.owl-carousel").owlCarousel({
                 nav: true, // Show next/prev buttons.
                 items: 3, // The number of items you want to see on the screen.
@@ -194,20 +193,20 @@
                 navText: false,
                 autoHeight: true,
                 margin: 25,
-                responsive : {
+                responsive: {
                     // breakpoint from 0 up
-                    0 : {
+                    0: {
                         items: 1,
                     },
                     // breakpoint from 480 up
-                    480 : {
+                    480: {
                         items: 1,
                     },
                     // breakpoint from 768 up
-                    768 : {
+                    768: {
                         items: 2,
                     },
-                    1200 : {
+                    1200: {
                         items: 2,
                     }
                 }
@@ -222,16 +221,16 @@
             navText: false,
             margin: 10,
             autoHeight: true,
-            responsive : {
+            responsive: {
                 // breakpoint from 0 up
-                0 : {
+                0: {
                     items: 2,
                 },
                 // breakpoint from 768 up
-                768 : {
+                768: {
                     items: 4,
                 },
-                1200 : {
+                1200: {
                     items: 5,
                 }
             }
@@ -241,11 +240,11 @@
         //Form Controls
         $('.form-control')
             .val('')
-            .on("focusin", function(){
+            .on("focusin", function () {
                 $(this).parent('.form-group').addClass('form-group-focus');
             })
-            .on("focusout", function(){
-                if($(this).val().length === 0) {
+            .on("focusout", function () {
+                if ($(this).val().length === 0) {
                     $(this).parent('.form-group').removeClass('form-group-focus');
                 }
             });
@@ -268,31 +267,31 @@
             },
 
             iframe: {
-                markup: '<div class="mfp-iframe-scaler">'+
-                        '<div class="mfp-close"></div>'+
-                        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                        '<div class="mfp-title mfp-bottom-iframe-title"></div>'+
-                      '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+                markup: '<div class="mfp-iframe-scaler">' +
+                    '<div class="mfp-close"></div>' +
+                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                    '<div class="mfp-title mfp-bottom-iframe-title"></div>' +
+                    '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
 
                 patterns: {
                     youtube: {
-                      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+                        index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
 
-                      id: null, // String that splits URL in a two parts, second part should be %id%
-                      // Or null - full URL will be returned
-                      // Or a function that should return %id%, for example:
-                      // id: function(url) { return 'parsed id'; }
+                        id: null, // String that splits URL in a two parts, second part should be %id%
+                        // Or null - full URL will be returned
+                        // Or a function that should return %id%, for example:
+                        // id: function(url) { return 'parsed id'; }
 
-                      src: '%id%?autoplay=1' // URL that will be set as a source for iframe.
+                        src: '%id%?autoplay=1' // URL that will be set as a source for iframe.
                     },
                     vimeo: {
-                      index: 'vimeo.com/',
-                      id: '/',
-                      src: '//player.vimeo.com/video/%id%?autoplay=1'
+                        index: 'vimeo.com/',
+                        id: '/',
+                        src: '//player.vimeo.com/video/%id%?autoplay=1'
                     },
                     gmaps: {
-                      index: '//maps.google.',
-                      src: '%id%&output=embed'
+                        index: '//maps.google.',
+                        src: '%id%&output=embed'
                     }
                 },
 
@@ -300,19 +299,40 @@
             },
 
             callbacks: {
-                markupParse: function(template, values, item) {
-                 values.title = item.el.attr('title');
+                markupParse: function (template, values, item) {
+                    values.title = item.el.attr('title');
                 }
             },
         });
 
         //Google Maps
         $("#map").googleMap({
-            zoom: 16 // Google Map ZOOM. You can change this value
+            zoom: 17 // Google Map ZOOM. You can change this value
         });
         $("#map").addMarker({
-            address: "S601 Townsend Street, San Francisco, California, USA", // Your Address. Change it
+            address: "Dawam MyID, Jakarta Barat, Jakarta, Indonesia", // Your Address. Change it
         });
     });
+
+    // Get Elements
+    const previewImg = document.querySelectorAll('.img-wrapper img');
+    const modalImg = document.querySelector('.modal-img img');
+    const modal = document.querySelector('.modal');
+
+    previewImg.forEach((images) => {
+        images.addEventListener('click', () => {
+            modal.classList.add('open');
+            let imgSrc = images.src;
+            modalImg.src = imgSrc;
+            console.log(modalImg.src);
+        });
+    });
+
+    modal.addEventListener('click', e => {
+        if (e.target.classList.contains('modal')) {
+            modal.classList.remove('open');
+        }
+    });
+
 
 })(jQuery);
